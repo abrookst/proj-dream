acd457bfeda6131d61be42e1027a12b345e92605
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-06 22:08:30 -0400

    Implement Block class and Set/GetBlock() in Entity

---
+++ game/src/action/block.h
@@ -0,0 +1,14 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include "action.h"
+#include "../entity/entity.h"
+
+class Block : protected Action
+{
+public:
+    void Preform(
+            Entity);
+};
+
+#endif
