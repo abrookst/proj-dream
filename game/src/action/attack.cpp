8c2a4853a084c9d6f020a64c157277f02015530e
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-07 21:52:38 -0400

    Add blocking logic to Attack to reduce damage to 1

--- game/src/action/attack.cpp
+++ game/src/action/attack.cpp
@@ -10,6 +10,7 @@ void Attack::Perform(
         (rand() % 100 > e1.GetAccuracy() 
          ? 0 
          : 1);
+    if (e2.GetBlock()) { attackerPower = 1; }
     e2.SetHealth(e2.GetHealth() - attackerPower);
 }
 
