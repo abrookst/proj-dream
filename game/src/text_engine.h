aea83aaffb6cf8595c9a701cf002441d383f2e4f
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-11 17:54:43 -0400

    Update header file

--- game/src/text_engine.h
+++ game/src/text_engine.h
@@ -1,6 +1,7 @@
 #ifndef TEXT_ENGINE_H
 #define TEXT_ENGINE_H 
 
+#include <queue>
 #include <utility>
 #include <string.h>
 #include <stdio.h>
@@ -9,10 +10,24 @@
 
 class TextEngine
 {
-    std::string writeBuffer;
+    char* finalBuffer;
+    std::queue<std::string*> writeQueue;
+    uint8_t delay = 0;
+    uint8_t pause = 0;
+    uint8_t frameCount = 0;
+    uint8_t pauseCount = 0;
+    uint8_t lineState = 2;
+
+    std::string FormatText(const std::string&);
 public:
+    TextEngine(Font&);
+    ~TextEngine();
     void Write(const std::string&);
-    bool IsDone();
+    bool IsDone() { return writeQueue.empty(); }
+    uint8_t GetLineState() { return lineState; }
+    void SetDelay(uint8_t d) { delay = d; }
+    void SetPause(uint8_t p) { pause = p; }
+    void UpdateText(bool);
 };
 
 
