8e4519451d45106de68c6c0f7e0e3bbbf4fc0613
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-20 20:48:13 -0400

    Move implementation from individual .cpp files to action.cpp

--- game/src/action.cpp
+++ game/src/action.cpp
@@ -32,8 +32,20 @@ void Other(Entity& e1, Entity& e2, Action act, TextEngine& textEngine)
         case HEALTH_POTION:
             break;
         case MAGIC_BLAST:
-            break;
-        default:
+            uint8_t attackerPower = 2 * e1.GetAttackPower() *
+                (rand() % 100 > e1.GetAccuracy()
+                 ? 0
+                 : 1);
+            if (e2.GetBlock()) { attackerPower = 1; }
+
+            int16_t newMana = e1.GetMana() - 2;
+            if (newMana < 0) { e1.SetMana(0); }
+            else { e1.SetMana(newMana); }
+
+
+            int16_t newHealth = e2.GetHealth() - attackerPower;
+            if (newHealth < 0) { e2.SetHealth(0); }
+            else { e2.SetHealth(newHealth); }
             break;
     }
 }
